526c871742f403cb6dbeeb72829b5aa8cceebe69
Rodrigo Ayarza <rayarza@nautilus.cl>
2025-05-25 13:37:01 -0400

    contador_click

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup>
-import HeaderComponent from "./components/HeaderComponent.vue"
-import { ref } from 'vue';
+import { ref } from "vue";
+const number = ref(1);
 
-    let number = ref(0);
-    setInterval(()=>{
-      number.value++;
-    }, 1000);
+const addOne = () => {
+  number.value++;
+}
 </script>
 
-
 <template>
-  <HeaderComponent></HeaderComponent>
-  <h1 class="title">Hola a todos!</h1>
-  <h2>{{  number }}</h2>
+  <h1>Hola a todos!</h1>
+  <h2 @click="addOne">{{  number }}</h2>
 </template>
 
-
-<style></style>
+<style>
+h1 {
+    color: red;
+}
+h2 {
+  color: rgb(9, 0, 128);
+}
+</style>
 
