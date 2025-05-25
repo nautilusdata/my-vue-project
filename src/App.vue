a2af531df67c683ad943c8fb2ccc6df81b6aac58
Rodrigo Ayarza <rayarza@nautilus.cl>
2025-05-25 01:47:14 -0400

    contador

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup>
 import HeaderComponent from "./components/HeaderComponent.vue"
+import { ref } from 'vue';
+
+    let number = ref(0);
+    setInterval(()=>{
+      number.value++;
+    }, 1000);
 </script>
 
 
 <template>
   <HeaderComponent></HeaderComponent>
   <h1 class="title">Hola a todos!</h1>
+  <h2>{{  number }}</h2>
 </template>
 
 
-<style>
-  .title {
-    background-color:rgb(114, 127, 163);
-    color: rgb(14, 50, 211);
-  }
-</style>
+<style></style>
 
