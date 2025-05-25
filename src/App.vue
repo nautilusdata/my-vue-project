4f80d7ecc116be74c3b59047c161b40a8512fd0b
Rodrigo Ayarza <rayarza@nautilus.cl>
2025-05-24 23:49:03 -0400

    Initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+
+<template>
+  <h1 class="title">Hola a todos!</h1>
+</template>
+
+
+<style>
+  .title {
+    background-color:rgb(7, 52, 177);
+    color: rgb(109, 211, 14);
+  }
+</style>
+
